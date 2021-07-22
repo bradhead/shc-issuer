506869031b1bdffccdcfd011521594ce3beda448
bradhead <brad@zedwerks.com>
2021-07-22 15:52:44 -0700

    updates

--- Services/ShcIssuer/src/Controllers/SmartHealthCardController.cs
+++ Services/ShcIssuer/src/Controllers/SmartHealthCardController.cs
@@ -2,29 +2,59 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net.Mime;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 using ShcIssuer.Models;
+using ShcIssuer.Models.Multipart;
+using ShcIssuer.Services;
 
 namespace ShcIssuer.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class SmartHealthCardController : ControllerBase
     {
+        private readonly ILogger<SmartHealthCardController> logger;
+        private readonly ISmartHealthCardService service;
 
-        private readonly ILogger<SmartHealthCardController> _logger;
 
-        public SmartHealthCardController(ILogger<SmartHealthCardController> logger)
+        public SmartHealthCardController(ILogger<SmartHealthCardController> logger,
+            ISmartHealthCardService service)
         {
-            _logger = logger;
+            this.service = service;
+            this.logger = logger;
         }
 
-        [HttpGet]
-        public SmartHealthCardBundle Get()
+        [HttpPost]
+        [Route("/Patient/{id}/$smart-health-card-bundle")]
+        public IActionResult CreateSmartHealthCardBundle([FromBody] HealthCardIssueRequest request, string id)
         {
-            return new SmartHealthCardBundle();
+
+            return new MultipartResult()
+            {
+                new MultipartEntry()
+                {
+                    ContentType = "application/smart-health-card",
+                    FileName = "SmartHealthCard.smart-health-card",
+                    Stream = null,
+                },
+                new MultipartEntry()
+                {
+                    ContentType = "image/png",
+                    FileName = "smart-health-card.png",
+                    Stream = null,
+                }
+            };
+        }
+
+        [HttpPost]
+        [Route("/Patient/{id}/$health-cards-issue")]
+        [Produces("application/smart-health-card")]
+        public async Task<ActionResult<SmartHealthCardModel>> CreateSmartHealthCard([FromBody] HealthCardIssueRequest request, string id)
+        {
+            return new SmartHealthCardModel();
         }
     }
 }
